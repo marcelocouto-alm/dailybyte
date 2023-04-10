7aed283349506b22aa5d401440b4b945aa283f63
marcelocouto.alm <marcelocouto.alm@gmail.com>
2023-04-10 18:30:35 -0300

    Adiciona mensagem de loading antes de exibir os posts

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -7,9 +7,12 @@ export default function Home() {
 
   // Estado para armazenar os posts
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   // Função assíncrona para buscar posts, usuários e comentários da API
   async function getPosts() {
+    setIsLoading(true);
+
     const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts');
     const postsData = await postsResponse.json();
 
@@ -33,7 +36,10 @@ export default function Home() {
     });
 
     // Atualiza o estado com os posts atualizados
-    setPosts(updatedPosts);
+    setTimeout(() => {
+      setIsLoading(false);
+      setPosts(updatedPosts);
+    }, 4000);
   }
 
   // useEffect para chamar a função getPosts na montagem do componente
@@ -44,10 +50,18 @@ export default function Home() {
   return (
     <div className="container">
       <Header />
-      <main className="content">
-        <h2 className="sub-title">Desenvolvido por <a href="https://marcelo-couto.web.app/" target="_blank"> Marcelo Couto</a></h2>
-        <PostList posts={posts} />
-      </main>
+      {isLoading ? (
+        <div className="loader">
+          <span class="loader-text">Carregando</span>
+          <span class="load"></span>
+        </div>
+      ) : (
+        <main className="content">
+          <h2 className="sub-title">Desenvolvido por <a href="https://marcelo-couto.web.app/" target="_blank"> Marcelo Couto</a></h2>
+          <PostList posts={posts} />
+        </main>
+      )}
     </div>
   )
+
 }
